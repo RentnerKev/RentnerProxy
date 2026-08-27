8f5eec36242ca55118ce54ea262bccf1318216c8
HomePC\Kevin <kevin@straessler-privat.de>
2026-08-27 19:57:45 +0200

    feat(web): add global recovery pages

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,9 +1,13 @@
 import { createRouter } from '@tanstack/react-router'
+import { GlobalErrorPage, NotFoundPage } from './components/system-state-page'
 import { routeTree } from './routeTree.gen'
 
 export function getRouter() {
   return createRouter({
     routeTree,
     scrollRestoration: true,
+    notFoundMode: 'root',
+    defaultNotFoundComponent: NotFoundPage,
+    defaultErrorComponent: GlobalErrorPage,
   })
 }
